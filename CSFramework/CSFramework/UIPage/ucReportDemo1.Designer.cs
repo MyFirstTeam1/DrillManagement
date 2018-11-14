3903e4b6964c9a09ed48d9f0055fb52c01e9923c
TerryTanLL <tanleliang@163.com>
2018-11-14 21:46:59 +0800

    test
    test commit

--- CSFramework/CSFramework/UIPage/ucReportDemo1.Designer.cs
+++ CSFramework/CSFramework/UIPage/ucReportDemo1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,13 +46,33 @@
             this.button1.Text = "报表样本一";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(87, 56);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(844, 413);
+            this.gridControl1.TabIndex = 2;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.Hidden;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
             // ucReportDemo1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridControl1);
             this.Controls.Add(this.button1);
             this.Name = "ucReportDemo1";
             this.Size = new System.Drawing.Size(968, 526);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
